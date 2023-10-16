cb5022a8fc5658d678227f9e7f43a2c5636877a5
Vijay <vijay.annamalai001@gmail.com>
2023-10-16 20:01:09 +0530

    feature 2: added mobiles in data.js

--- scripts/data.js
+++ scripts/data.js
@@ -26,14 +26,98 @@ let itemsData=[
     },
     { id:"samsung1",
     productName:"Samsung Galaxy S22 Ultra 5G ",
-    price:79999,
-    mrp:99999,
+    price:109999,
+    mrp:129999,
     img:"assets/samsung1.jpg"
     },
     { id:"samsung2",
     productName:"Samsung Galaxy S23 5G ",
+    price:129999,
+    mrp:139999,
+    img:"assets/samsung2.jpg"
+    },
+    { id:"samsung3",
+    productName:"Samsung Galaxy S23 FE 5G ",
+    price:125999,
+    mrp:135999,
+    img:"assets/samsung3.jpg"
+    },
+    { id:"samsung4",
+    productName:"Samsung Galaxy M14 5G ",
     price:79999,
     mrp:99999,
-    img:"assets/samsung2.jpg"
+    img:"assets/samsung4.jpg"
+    },
+    { id:"redmi1",
+    productName:"Redmi 10 Power ",
+    price:15999,
+    mrp:17999,
+    img:"assets/redmi1.jpg"
+    },
+    { id:"redmi2",
+    productName:"Redmi 11 ",
+    price:20999,
+    mrp:23999,
+    img:"assets/redmi2.jpg"
+    },
+    { id:"redmi3",
+    productName:"Redmi A2 ",
+    price:10999,
+    mrp:12999,
+    img:"assets/redmi3.jpg"
+    },
+    { id:"redmi4",
+    productName:"Redmi 12 ",
+    price:22999,
+    mrp:25999,
+    img:"assets/redmi4.jpg"
+    },
+    { id:"realme1",
+    productName:"Realme Narzo 60 pro ",
+    price:17999,
+    mrp:19999,
+    img:"assets/realme1.jpg"
+    },
+    { id:"realme2",
+    productName:"Realme Narzo 60 X ",
+    price:18999,
+    mrp:20999,
+    img:"assets/realme2.jpg"
+    },
+    { id:"realme3",
+    productName:"Realme Narzo 50A ",
+    price:10999,
+    mrp:11999,
+    img:"assets/realme3.jpg"
+    },
+    { id:"realme4",
+    productName:"Realme Narzo N53 ",
+    price:23999,
+    mrp:24999,
+    img:"assets/realme4.jpg"
+    },
+    { id:"basic1",
+    productName:"Lava - Charcoal ",
+    price:1500,
+    mrp:2100,
+    img:"assets/basic1.jpg"
+    },
+    { id:"basic2",
+    productName:"Lava - white ",
+    price:1700,
+    mrp:2500,
+    img:"assets/basic2.jpg"
+    },
+    { id:"basic3",
+    productName:"Lava - black ",
+    price:2000,
+    mrp:2500,
+    img:"assets/basic3.jpg"
+    },
+    { id:"basic4",
+    productName:"Motorola ",
+    price:2500,
+    mrp:3000,
+    img:"assets/basic4.jpg"
     }
     ]
